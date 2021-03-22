0a6a21910cd6431304198084bd4e677e954361f0
ozkknnt <ozkknnt@users.noreply.github.com>
2021-03-22 23:33:31 +0900

    カスタムディレクティブの作り方

--- vuecli/udemy-vuejs/src/main.js
+++ vuecli/udemy-vuejs/src/main.js
@@ -4,6 +4,26 @@ import LikeNumber from "./components/LikeNumber"
 
 Vue.config.productionTip = false
 Vue.component("LikeNumber", LikeNumber);
+// Vue.directive("border", {
+//   bind(el, binding, vnode) {
+//     // ディレクティブに初めて対象の要素に紐付いたとき
+//   },
+//   inserted(el, binding, vnode) {
+//     // 親Nodeに挿入されたとき
+//   },
+//   update(el, binding, vnode, oldvnode) {
+//     // コンポーネントが更新されるたび、子コンポーネントが更新される前
+//   },
+//   componentUpdated(el, binding, vnode, oldvnode) {
+//     // コンポーネントが更新されるたび、子コンポーネントが更新された後
+//   },
+//   unbind(el, binding, vnode) {
+//     // ディレクティブが紐付いている要素から取り除かれたとき
+//   }
+// });
+
+// 上記のbindとupdateの省略記法
+Vue.directive("border", function(el, binding){});
 
 new Vue({
   render: h => h(App),
