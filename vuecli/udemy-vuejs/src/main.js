06e32a34ad7bdc7d36df7e1ceca51fdc4c33d602
ozkknnt <ozkknnt@users.noreply.github.com>
2021-03-25 23:53:59 +0900

    カスタムディレクティブ終了

--- vuecli/udemy-vuejs/src/main.js
+++ vuecli/udemy-vuejs/src/main.js
@@ -23,7 +23,21 @@ Vue.component("LikeNumber", LikeNumber);
 // });
 
 // 上記のbindとupdateの省略記法
-Vue.directive("border", function(el, binding){});
+
+
+// グローバルのカスタムディレクティブ
+// Vue.directive("border", function (el, binding) {
+//   // el.style.border = "solid black 2px";
+//   el.style.borderWidth = binding.value.width;
+//   el.style.borderColor = binding.value.color;
+//   el.style.borderStyle = binding.arg;
+//   if (binding.modifiers.round) {
+//     el.style.borderRadius = "0.5rem"
+//   }
+//   if (binding.modifiers.shadow) {
+//     el.style.boxShadow = "0 2px 5px rgba(0,0,0,0.26)"
+//   }
+// });
 
 new Vue({
   render: h => h(App),
