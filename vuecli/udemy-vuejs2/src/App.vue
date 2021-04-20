c5147cfa511448eef5950c8c8190e8f65326a65a
ozkknnt <25022317+ozkknnt@users.noreply.github.com>
2021-04-21 00:01:28 +0900

    transition

---
+++ vuecli/udemy-vuejs2/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main">
+    <button @click="show = !show">切り替え</button>
+    <transition name="fade">
+      <p v-if="show">hello</p>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: true
+    };
+  }
+}
+</script>
+
+<style>
+.fade-enter{}
+.fade-enter-active{}
+.fade-enter-to{}
+.fade-enter{}
+.fade-leave{}
+.fade-leave-active{}
+.fade-leave-to{}
+.main {
+  width: 70%;
+  mergin: auto;
+  padding-top: 5rem;
+  text-align: center;
+}
+</style>
